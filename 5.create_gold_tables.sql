74a6bd414b0674de0fc7a1f6d3bf064c6c17e35d
jonesjust <jonesjust>
2023-08-03 14:43:43 +0000

    create bronze, silver, and gold tables

---
+++ 5.create_gold_tables.sql
@@ -0,0 +1,19 @@
+-- Databricks notebook source
+DROP TABLE IF EXISTS formula1_dev.gold.driver_wins;
+
+CREATE TABLE formula1_dev.gold.driver_wins
+AS
+SELECT 
+  d.name, count(1) AS number_of_wins
+FROM formula1_dev.silver.drivers d
+JOIN formula1_dev.silver.results r
+ON (d.driver_id = r.driver_id)
+WHERE r.position = 1
+GROUP BY d.name;
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM formula1_dev.gold.driver_wins 
+ORDER BY number_of_wins DESC;
